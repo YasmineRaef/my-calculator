1bf7685a8a9c73753a748b801e96e9e362f5007d
YasmineRaef <yasmineraef76@gmail.com>
2023-10-06 01:23:30 +0300

    Modified Calculator

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:my_calculator/main.dart';
+import 'package:my_calculator/calculator_screen.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
